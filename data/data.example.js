17b50ed369fcd0073ef47e09ae937df1472946ba
Arjun <arjun.p.dhaliwal@gmail.com>
2017-09-09 16:13:00 -0400

    some restructuring and git ignoring

--- data/data.example.js
+++ data/data.example.js
@@ -1,7 +1,7 @@
 'use strict';
 var toEmail = '';
 var fromEmail = ''
-var subreddits = ['soccer', 'videos', 'Showerthoughts', 'space', 'AskReddit', 'gifs'];
+var subreddits = [];
 
 module.exports = {
 	toEmail: toEmail,
